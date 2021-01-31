8b9d9b10e080d3dc275ce2f66cb2f2488b8458ad
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-01-31 22:09:55 +0600

    site completed

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
-const generateBtn = document.getElementById('generate-btn');
-generateBtn.addEventListener('click', function () {
-    const generate = Math.floor(1000 + Math.random() * 9000);
+// Pin Generator
+function generatePin() {
+    const generate = Math.floor(1000 + Math.random() * 9000) + '';
     document.getElementById('generate-input').value = generate;
+}''
+// handle calculator event
+const buttonContainer = document.getElementById('digits-container');
+buttonContainer.addEventListener('click', function (event) {
+    const digit = event.target.innerText;
+    if (isNaN(digit)) {
+        if (digit === 'C') {
+            const typedInput = document.getElementById("typed-pin");
+            typedInput.value = '';
+        }
+    } else {
+        const typedInput = document.getElementById("typed-pin");
+        typedInput.value = typedInput.value + digit;
+    }
 });
+// Match Pin
+function matchPin() {
+    const pin = document.getElementById('generate-input').value;
+    const typePin = document.getElementById("typed-pin").value;
+    if (pin === typePin) {
+        displayMatchResult('block', 'none');
+    } else {
+        displayMatchResult('none' ,'block');
+    }
+};
+// display match result
+function displayMatchResult(correctStatus, inCorrectStatus){
+    const correct = document.getElementById('correct-pin');
+    correct.style.display = correctStatus;
+    const incorrect = document.getElementById('incorrect-pin');
+    incorrect.style.display = inCorrectStatus;
+};
